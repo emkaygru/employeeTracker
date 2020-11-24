e6e5e8c3853c72839c25e5744b1ad881ddda8a99
Emily Groenendal <emilylauder@gmail.com>
2020-11-24 17:16:41 -0500

    upating inquirer prompts

--- app.js
+++ app.js
@@ -4,3 +4,33 @@ const cTable = require('console.table');
 const faker = require('faker');
 const mysql = require('mysql');
 
+function vaidateDChoice(){
+    if (choice === 'department'){
+        return true; 
+    }
+}
+
+function addDRE{
+
+    inquirer.prompt([
+        {
+            type: 'confirm',
+            message: 'Would you like to add a new department?',
+            name: 'dChoice',
+
+        },
+        {
+            type: 'confirm',
+            message: 'Would you like to add a new role?',
+            name: 'rChoice'
+        },
+        {
+            type: 'confirm',
+            message: 'Would you like to add a new employee?',
+            name: 'eChoice'
+        }
+        
+    ]);
+
+
+}
