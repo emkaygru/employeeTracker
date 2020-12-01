6d23ab1e975009587c59e94a85840a6708d489f6
Emily Groenendal <emilylauder@gmail.com>
2020-11-30 21:44:58 -0500

    creating functions for console table

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const inquirer = require('inquirer');
 const cTable = require('console.table');
 
 const mysql = require('mysql');
+const { table } = require('console');
 
 
 
@@ -59,4 +60,33 @@ function startScreen(){
 
 };
 
-// Function calls 
+// Function calls ----------------------------------------------------------------
+
+
+function viewAllEmployees(){
+    connection.query('SELECT employee.first_name, employee.last_name, role.title, role.salary, department.name, CONCAT(e.first_name,' ', e.last_name) AS Manager FROM employee INNER JOIN role on role.id = employee.role_id INNER JOIN department on department.id = role.department_id left join employee e on employee.manager_id = e.id;',
+    function(err,res){ 
+        if(err) throw err;
+        console.table(res)
+        startScreen();
+
+    })
+};
+
+function viewAllEmployeesByRoles(){
+    connection.query('SELECT employee.first_name, employee.last_name, role.title AS Title FROM employee JOIN role ON employee.role_id = role.id;',
+    function(err,res){
+        if(err) throw err;
+        console.table(res)
+        startScreen();
+    })
+};
+
+function viewAllEmployeesByDepartments(){
+    connection.query('SELECT employee.first_name, employee.last_name, department.name AS Department FROM employee JOIN role ON employee.role_id = role.id JOIN department ON role.department_id = department.id ORDER BY employee.id;',
+    function(err,res){
+        if(err) throw err;
+        console.table(res)
+        startScreen();
+    })
+};
