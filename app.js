e33c6d74d4d257b7a5eb9cd69e1b9dab0826f955
Emily Groenendal <emilylauder@gmail.com>
2020-12-02 19:43:31 -0500

    updating inquirer functions

--- app.js
+++ app.js
@@ -1,107 +1,109 @@
-const fs = require('fs');
-const inquirer = require('inquirer');
-const cTable = require('console.table');
+const fs = require("fs");
+const inquirer = require("inquirer");
+const cTable = require("console.table");
 
-const mysql = require('mysql');
-const { table } = require('console');
+const mysql = require("mysql");
+const { table } = require("console");
 
+function startScreen() {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        message: "Would you like to do?",
+        name: "choice",
+        choices: [
+          "View All Employees?",
+          "View All Employee's By Roles?",
+          "View all Employees By Departments",
+          "Update Employee",
+          "Add Employee?",
+          "Add Role?",
+          "Add Department?",
+        ],
+      },
+    ])
+    .then(function (result) {
+      switch (result.choice) {
+        case "View All Employees":
+          viewAllEmployees();
+          break;
 
+        case "View All Employees by Roles":
+          viewAllEmployeesByRoles();
+          break;
 
-function startScreen(){
+        case "View All Employees by Departments":
+          viewAllEmployeesByDepartments();
+          break;
 
-    inquirer.prompt([
-        {
-            type: 'list',
-            message: 'Would you like to do?',
-            name: 'choice',
-            choices: [
-             "View All Employees?", 
-              "View All Employee's By Roles?",
-              "View all Employees By Departments", 
-              "Update Employee",
-              "Add Employee?",
-              "Add Role?",
-              "Add Department?"
-            ]
-    
-        }
+        case "Update Employee":
+          updateEmployee();
+          break;
 
-    ]).then(function(result){
-        switch (result.choice){
-            case "View All Employees":
-            viewAllEmployees();
-            break;
+        case "Add Employee":
+          addEmployee();
+          break;
 
-            case "View All Employees by Roles":
-            viewAllEmployeesByRoles();
-            break;
+        case "Add Role":
+          addRole();
+          break;
 
-            case "View All Employees by Departments":
-            viewAllEmployeesByDepartments();
-            break;
-
-            case "Update Employee":
-            updateEmployee();
-            break;
-
-            case "Add Employee":
-            addEmployee();
-            break;
-
-            case "Add Role":
-            addRole();
-            break;
-
-            case "Add Department":
-            addDepartment();
-            break;
-        }
-    })
-
-};
+        case "Add Department":
+          addDepartment();
+          break;
+      }
+    });
+}
 
 // Function calls ----------------------------------------------------------------
 
+function viewAllEmployees() {
+  connection.query("SELECT * FROM employee", function (err, res) {
+    if (err) throw err;
+    console.table(res);
+    startScreen();
+  });
+}
 
-function viewAllEmployees(){
-    connection.query('SELECT employee.first_name, employee.last_name, role.title, role.salary, department.name, CONCAT(e.first_name,' ', e.last_name) AS Manager FROM employee INNER JOIN role on role.id = employee.role_id INNER JOIN department on department.id = role.department_id left join employee e on employee.manager_id = e.id;',
-    function(err,res){ 
-        if(err) throw err;
-        console.table(res)
-        startScreen();
+function viewAllEmployeesByRoles() {
+  connection.query("SELECT * FROM role", function (err, res) {
+    if (err) throw err;
+    console.table(res);
+    startScreen();
+  });
+}
 
-    })
-};
+function viewAllEmployeesByDepartments() {
+  connection.query("SELECT * FROM department", function (err, res) {
+    if (err) throw err;
+    console.table(res);
+    startScreen();
+  });
+}
 
-function viewAllEmployeesByRoles(){
-    connection.query('SELECT employee.first_name, employee.last_name, role.title AS Title FROM employee JOIN role ON employee.role_id = role.id;',
-    function(err,res){
-        if(err) throw err;
-        console.table(res)
-        startScreen();
-    })
-};
-
-function viewAllEmployeesByDepartments(){
-    connection.query('SELECT employee.first_name, employee.last_name, department.name AS Department FROM employee JOIN role ON employee.role_id = role.id JOIN department ON role.department_id = department.id ORDER BY employee.id;',
-    function(err,res){
-        if(err) throw err;
-        console.table(res)
-        startScreen();
-    })
-};
-
-function updateEmployee(){
-    inquirer.prompt([{
-        type:'input', 
-        message: 'Which employee would you like to update?',
-        name: 'employeeUpdate'
-    },{
-        type:'input',
-        message: 'What do you want to update their role to?',
-        name: 'updateERole'
-    }
-    ]).then(function(comment){
-        
-    })
+function updateEmployee() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "Which employee would you like to update?",
+        name: "employeeUpdate",
+      },
+      {
+        type: "input",
+        message: "What do you want to update their role to?",
+        name: "updateERole",
+      },
+    ])
+    .then(function (answer) {
+      connection.query(
+        "UPDATE employee SET role_id=? WHERE first_name= ?",
+        [answer.updateERole, answer.employeeUpdate],
+        function (err, res) {
+          if (err) throw err;
+          console.log(res);
+        }
+      );
+    });
 }
